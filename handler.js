462f2854523f67b9a82c6f6f458c41671d9a39f3
Vu Minh Thang <vuminhthang.cm@gmail.com>
2017-12-01 17:53:10 +0900

    Init project with serverless

---
+++ handler.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports.hello = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Hello world!'
+    }),
+  };
+
+  callback(null, response);
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
