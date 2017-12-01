82c6f20c55cef20856eee065e221629bee9e53a5
Vu Minh Thang <vuminhthang.cm@gmail.com>
2017-12-01 18:51:06 +0900

    Update config

--- handler.js
+++ handler.js
@@ -3,14 +3,20 @@
 import RedisCache from './helper/RedisCache'
 
 module.exports.hello = async (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false
+
   const CACHE_KEY = 'CACHE_KEY'
-  await RedisCache.set(CACHE_KEY, {"message": "Hello World!"})
+  let res = {}
   let checkCache = await RedisCache.get(CACHE_KEY)
+  if (checkCache) {
+    res = checkCache
+  } else {
+    await RedisCache.set(CACHE_KEY, {"message": "Hello World!"})
+    res = {"message": "Set cache success!"}
+  }
   const response = {
     statusCode: 200,
-    body: JSON.stringify({
-      checkCache
-    }),
+    body: JSON.stringify(res),
   };
 
   callback(null, response);
